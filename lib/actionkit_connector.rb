995a1343d5779521be7809ecbfdaa5796c486019
Eric Boersma <eric.boersma@gmail.com>
2015-03-03 10:26:09 -0500

    Reworking AK connector to allow changing properties.

--- lib/actionkit_connector.rb
+++ lib/actionkit_connector.rb
@@ -4,15 +4,35 @@ module ActionKitConnector
   class Connector
     include HTTParty
 
+    attr_accessor :username
+    attr_accessor :password
+    attr_accessor :base_url
+
+    # Initializes a connector to the ActionKit API.
+    # A new connection is created on each call, so there
+    # no need to worry about resource utilization.
+    #
+    # @param [String] username The username of your ActionKit user.
+    # @param [String] password The password for your ActionKit user.
+    # @param [String] base_url The base url of your ActionKit instance.
     def initialize(username, password, base_url)
-      @auth = {username: username, password: password}
-      @base_url = base_url
+      self.username = username
+      self.password = password
+      self.base_url = base_url
     end
 
+    def auth
+      {username: self.username, password: self.password}
+    end
+
+    # Lists petition pages in your instance.
+    #
+    # @param [Int] offset The number of records to skip.
+    # @param [Int] limit  The maximum number of results to return.
     def list_petition_pages(offset=0, limit=20)
-      target = "#{@base_url}/petitionpage/"
+      target = "#{self.base_url}/petitionpage/"
       options = {
-          basic_auth: @auth,
+          basic_auth: self.auth,
           query: {
               _limit: limit,
               _offset: offset
@@ -21,15 +41,24 @@ module ActionKitConnector
       self.class.get(target, options)
     end
 
+    # Returns the information for a single PetitionPage.
+    #
+    # @param [Int] id The ID of the page to return.
     def petition_page(id)
-      target = "#{@base_url}/petitionpage/#{id}/"
-      self.class.get(target, {basic_auth: @auth})
+      target = "#{self.base_url}/petitionpage/#{id}/"
+      self.class.get(target, {basic_auth: self.auth})
     end
 
+    # Create a petition page in your ActionKit instance.
+    #
+    # @param [String] name          The name of the page.
+    # @param [String] title         The title of the page.
+    # @param [URI]    lang          The URI string for the language of this page in the form of /rest/v1/language/{id}
+    # @param [URL]    canonical_url The canonical URL for this page.
     def create_petition_page(name, title, lang, canonical_url)
-      target = "#{@base_url}/petitionpage/"
+      target = "#{self.base_url}/petitionpage/"
       options = {
-          basic_auth: @auth,
+          basic_auth: self.auth,
           headers: {
               'Content-type' => 'application/json'
           },
@@ -46,10 +75,14 @@ module ActionKitConnector
       self.class.post(target, options)
     end
 
+    # Creates an action which associates a user with a page.
+    #
+    # @param [String] page_name The ActionKit name of the page on which the action is being taken.
+    # @param [String] email     The email address of the person taking action.
     def create_action(page_name, email)
-      target = "#{@base_url}/action/"
+      target = "#{self.base_url}/action/"
       options = {
-          basic_auth: @auth,
+          basic_auth: self.auth,
           body: {
               page: page_name,
               email: email
